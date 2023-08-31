03d27cc70aeb91aeaf2adc7a66588bf80de31ab0
Anthonia Nwanya <tonianwanya@gmail.com~>
2023-08-31 22:58:35 +0100

    feat: run server

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const{ MongoDB }= require("./config/database");
+const ErrorHandler = require("./middleware/ErrorHandler");
+const AccountRoute = require("./route/AccountRoute")
+const PORT = process.env.PORT || 8000;
+const app = express();
+MongoDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/flutterwave", AccountRoute);
+
+app.use(function (err, req, res, next) {
+    ErrorHandler(err, req, res);
+  });
+
+app.listen(PORT, () => {
+    console.log(`Server started listening on ${PORT}`)
+})
