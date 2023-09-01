b905578efede513cc45a331f41f0dfd44b602c6d
Anthonia Nwanya <tonianwanya@gmail.com~>
2023-09-01 01:15:42 +0100

    fix: require dotenv

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+require('dotenv').config();
 const express = require("express");
 const{ MongoDB }= require("./config/database");
 const ErrorHandler = require("./middleware/ErrorHandler");
-const AccountRoute = require("./route/AccountRoute")
+const AccountRoute = require("./route/AccountRoute");
+
 const PORT = process.env.PORT || 8000;
 const app = express();
+
 MongoDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/flutterwave", AccountRoute);
+app.use("/flutter", AccountRoute);
 
 app.use(function (err, req, res, next) {
     ErrorHandler(err, req, res);
